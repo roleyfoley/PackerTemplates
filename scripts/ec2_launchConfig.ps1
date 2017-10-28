07a2bedd8bcef7aac5fca75c0f4da87652dfe120
roleyfoley <roleyfoley@gmail.com>
2017-10-29 10:42:42 +1100

    Fix custom unattend install overwrite

--- scripts/ec2_launchConfig.ps1
+++ scripts/ec2_launchConfig.ps1
@@ -18,6 +18,11 @@ $LaunchConfig.setWallpaper = $False
 
 Convertto-Json -Depth 100  $LaunchConfig | Out-File $ConfigFile
  
+# Check for custom unattend file - Copy from Temp 
+if ( test-path 'C:\Temp\unattend.xml' ) { 
+    Copy-Item 'C:\Temp\unattend.xml' 'C:\ProgramData\Amazon\EC2-Windows\Launch\Sysprep\Unattend.xml'
+}
+
 # Schedules the intial instance setup on the next boot
 C:\ProgramData\Amazon\EC2-Windows\Launch\Scripts\InitializeInstance.ps1 -Schedule
 
